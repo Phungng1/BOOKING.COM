5e76dca76dbcc94b66c5914ba11f6425f507842d
Phungng1 <148553404+Phungng1@users.noreply.github.com>
2024-04-12 18:30:22 +0700

    Xóa code cũ

--- SPCK/js/room.js
+++
@@ -1,7 +0,0 @@
-let getHotel = JSON.parse("hotel", localStorage(hotel))
-
-const divHanoi = document.getElementById("Hanoi")
-const divSaiGon = document.getElementById("SaiGon")
-const divDaNang = document.getElementById("DaNang")
-const divNhaTrang = document.getElementById("NhaTrang")
-const divHoiAn = document.getElementById("HoiAn")
